7cbcf677ff3a43e1b5fc6827e7bb8bbc57626d37
Logan Chook <loganchook@gmail.com>
2023-06-17 00:46:25 -0700

    hello

--- main.py
+++ main.py
@@ -4,7 +4,8 @@ def main():
     # make your code below
     # print('Hello World')
     ##############################
+    print ("Hello World")
+    print ("CS7: Introduction to Computer Programming")
 
-
-if __name__ == '__main__':
+if __name__=='__main__':
     main()
